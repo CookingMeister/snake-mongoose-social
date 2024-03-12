a45cf28c295aeede600d5e7a793b86fdd388fe21
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-03-12 11:59:26 -0300

    add push thought._id to each user thoughts.array in seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,7 @@
 import db from '../config/connection.js';
-import { User, Thought} from '../models/index.js';
+import { User, Thought } from '../models/index.js';
 
 db.once('open', async () => {
-
   // Delete existing documents
   await User.deleteMany({});
   await Thought.deleteMany({});
@@ -11,47 +10,61 @@ db.once('open', async () => {
   const users = [
     {
       username: 'uncleJohn',
-      email: 'john@email.com'
+      email: 'john@email.com',
     },
     {
       username: 'auntJane',
-      email: 'jane@email.com'
+      email: 'jane@email.com',
     },
     {
       username: 'uncleBob',
-      email: 'bob@email.com'
-    }
+      email: 'bob@email.com',
+    },
   ];
 
-  await User.collection.insertMany(users);
-
-  // Create sample thoughts
-  const thoughts = [
-    {
-      thoughtText: 'Here is a sample thought',
-      username: 'uncleJohn'
-    },
-    {
-      thoughtText: 'Here is a second sample thought',
-      username: 'auntJane'
-    },
-    {
-      thoughtText: 'Uncle John and Aunt Jane are the best!',
-      username: 'uncleBob'
-    }
+  const createdUsers = await User.collection.insertMany(users);
+  const userIdsArray = Object.values(createdUsers.insertedIds); //  Get Array of ObjectIds
+  const textArray = [
+    'Here is a sample thought',
+    'Here is a second sample thought',
+    'Uncle John and Aunt Jane are the best!',
   ];
 
-  await Thought.collection.insertMany(thoughts);
+  // Create thoughts and update users
+  for (let i = 0; i < userIdsArray.length; i++) {
+    const userId = userIdsArray[i];
+    const username = users[i].username;
+
+    const thought = {
+      thoughtText: textArray[i],
+      username,
+      userId,
+    };
+
+    const createdThought = await Thought.create(thought);
+
+    // Update user after thought creation
+    await User.updateOne(
+      { _id: userId },
+      { $push: { thoughts: createdThought._id } }
+    );
+  }
 
   // Add reactions to thoughts
   await Thought.updateOne(
-    {thoughtText: 'Here is a sample thought'}, 
-    {$push: {reactions: {reactionBody: 'Great thought!', username: 'auntJane'}}}
+    { thoughtText: 'Here is a sample thought' },
+    {
+      $push: {
+        reactions: { reactionBody: 'Great thought!', username: 'auntJane' },
+      },
+    }
   );
 
   await Thought.updateOne(
-    {thoughtText: 'Here is a second sample thought'},
-    {$push: {reactions: {reactionBody: 'Love it!', username: 'uncleJohn'}}}
+    { thoughtText: 'Here is a second sample thought' },
+    {
+      $push: { reactions: { reactionBody: 'Love it!', username: 'uncleJohn' } },
+    }
   );
 
   console.log('Database seeded!');
